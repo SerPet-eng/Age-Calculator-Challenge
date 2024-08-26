289daf4b8dab36090e200fe8810e54679a9db9c7
Christian De Guzman <62647434+SerPet-eng@users.noreply.github.com>
2024-08-26 18:04:56 +0800

    Update script.js 👷‍♂️🛠️
     - Adding/Modify Error Handling

--- script.js
+++ script.js
@@ -23,16 +23,22 @@ document.addEventListener('DOMContentLoaded', () => {
       resetErrors();
 
       if (dayValue < 1 || dayValue > 31) {
+        daysEl.textContent = '--';
+
         dayInput.classList.add('dayError');
         dayMessErr.classList.add('errorText');
         dayMessErr.textContent = 'Must be a valid day';
       }
       if (monthValue < 1 || monthValue > 12) {
+        monthsEl.textContent = '--';
+
         monthInput.classList.add('monthError');
         monthMessErr.classList.add('errorText');
         monthMessErr.textContent = 'Must be a valid month';
       }
       if (yearValue < 1940 || yearValue > 2099) {
+        yearsEl.textContent = '--';
+
         yearInput.classList.add('yearError');
         yearMessErr.classList.add('errorText');
         yearMessErr.textContent = 'Must be a valid year';
@@ -63,23 +69,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function getValue(days, months, years) {
     const currentDate = new Date();
-    let getDays = days - currentDate.getDate();
-    let getMonths = months - (currentDate.getMonth() + 1);
-    let getYears = years - currentDate.getFullYear();
 
-    if (!days) {
+    if (days && months && years) {
+      let getDays = days - currentDate.getDate();
+      let getMonths = months - (currentDate.getMonth() + 1);
+      let getYears = years - currentDate.getFullYear();
+
+      yearsEl.textContent = Math.abs(getYears);
+      monthsEl.textContent = Math.abs(getMonths);
+      daysEl.textContent = Math.abs(getDays);
+    } else {
       daysEl.textContent = '--';
-    }
-    if (!months) {
       monthsEl.textContent = '--';
-    }
-    if (!years) {
       yearsEl.textContent = '--';
     }
-
-    yearsEl.textContent = Math.abs(getYears);
-    monthsEl.textContent = Math.abs(getMonths);
-    daysEl.textContent = Math.abs(getDays);
   }
 
   resetErrors();
